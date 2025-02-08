6df1ef04e09c45fdc17886d324b06c45f522a1b0
styopashugh <styopashugh95@gamil.com>
2025-02-08 12:19:52 +0400

    commit

--- Coding Credit Card/script.js
+++ Coding Credit Card/script.js
@@ -1,50 +1,52 @@
-/////existedCard ={
-//cardType
-//cardNumber
-//cvvCard
-////card date
-//}
-// const existedCards = [];
-// const existedCard = document.getElementById("existedCard");
+// existedCard ={
+// cardType,
+// cardNumber,
+// cvvCard,
+// cardDate,
+// }
 
 
-// function printExistedCards() {
-//   existedCard.innerHTML = "";
-//   for (const element of existedCards) {
-//     for (const key in element) {
-//       if (element.hasOwnProperty( key)) {
-//         console.log(element[key]);
-//         const newElement = document.createElement("p")
-//         newElement.textContent = `${key}: ${element[key]}`;
-//         existedCard.appendChild(newElement);
-//         existedCard.classList.toggle('existedCard')
+const existedCards = [];
+const existedCard = document.getElementById("existedCard");
+
+
+function printExistedCards() {
+  existedCard.innerHTML = "";
+  for (const element of existedCards) {
+    for (const key in element) {
+      if (element.hasOwnProperty( key)) {
+        console.log(element[key]);
+        const newElement = document.createElement("p")
+        newElement.textContent = `${key}: ${element[key]}`;
+        existedCard.appendChild(newElement);
+        existedCard.classList.toggle('existedCard')
         
-//       }
-//     }
+      }
+    }
 
-//   }
-// }
+  }
+}
 
-// //
-// function makeCard() {
-//   const cardType = document.getElementById("cardType").value;
-//   // //////////////////////////////////////
-//   const cardNumber = document.getElementById("cardNumbers").value;
+//
+function makeCard() {
+  const cardType = document.getElementById("cardType").value;
+  // //////////////////////////////////////
+  const cardNumber = document.getElementById("cardNumbers").value;
 
-//   // ///////////////////////////////////////////
-//   const cvvNumber = document.getElementById("cvvNumber").value;
-//   console.log(cvvNumber.value);
-//   /////////////////////////////////////
-//   const cardDate = document.getElementById("dateInput").value;
+  // ///////////////////////////////////////////
+  const cvvNumber = document.getElementById("cvvNumber").value;
+  console.log(cvvNumber.value);
+  /////////////////////////////////////
+  const cardDate = document.getElementById("dateInput").value;
 
-//   ////////////////////////
-//   existedCards.push({
-//     cardType,
-//     cardNumber,
-//     cvvNumber,
-//     cardDate,
-//   });
-//   printExistedCards();
-// }
+  ////////////////////////
+  existedCards.push({
+    cardType,
+    cardNumber,
+    cvvNumber,
+    cardDate,
+  });
+  printExistedCards();
+}
 
 
